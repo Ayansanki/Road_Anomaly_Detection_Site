4b49daf693a5f0b00a13b3ae34a62b08a1e64470
Ayan Sanki <82274729+Ayansanki@users.noreply.github.com>
2025-11-23 00:37:11 +0530

    Update celery.py

--- road_anomaly_detection/celery.py
+++ road_anomaly_detection/celery.py
@@ -1,8 +1,1 @@
-import os
-from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'road_anomaly_detection.settings')
-
-app = Celery('road_anomaly_detection')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
